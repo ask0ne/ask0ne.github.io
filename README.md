840fdf7f5eddd513707385584c58040ab851c1ef
ask0ne <kawade.ak@gmail.com>
2021-04-15 17:33:55 +0530

    redesigned portfolio

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Personal Website
+
+<a href="https://iconscout.com/illustrations/concept" target="_blank">Concept of man got success and achieving goal Illustration</a> by <a href="https://iconscout.com/contributors/delesign">Delesign Graphics</a> on <a href="https://iconscout.com">Iconscout</a>
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,159 +1,57 @@
-/*
-	Read Only by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+var navbar = document.getElementById('navbar');
+var sticky = navbar.offsetTop;
+var about = document.getElementById('about');
+var skills = document.getElementById('skills');
+var projects = document.getElementById('projects');
+var achievements = document.getElementById('achievements');
 
-(function($) {
+about.style.display = 'none';
+skills.style.display = 'none';
+projects.style.display = 'none';
+achievements.style.display = 'none';
+window.onscroll = function () { stickNavbar() };
 
-	var $window = $(window),
-		$body = $('body'),
-		$header = $('#header'),
-		$titleBar = null,
-		$nav = $('#nav'),
-		$wrapper = $('#wrapper');
+function stickNavbar() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    }
+    else {
+        navbar.classList.remove('sticky');
+    }
+};
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '1025px',  '1280px' ],
-			medium:   [ '737px',   '1024px' ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ],
-		});
+function gototop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+function displayAbout() {
+    projects.style.display = 'none';
+    skills.style.display = 'none';
+    achievements.style.display = 'none';
+    about.style.display = 'block';
+    about.scrollIntoView({ behavior: 'smooth' });
+};
 
-	// Tweaks/fixes.
+function displaySkills() {
+    projects.style.display = 'none';
+    about.style.display = 'none';
+    achievements.style.display = 'none';
+    skills.style.display = 'block';
+    skills.scrollIntoView({ behavior: 'smooth' });
+};
 
-		// Polyfill: Object fit.
-			if (!browser.canUse('object-fit')) {
+function displayProjects() {
+    skills.style.display = 'none';
+    about.style.display = 'none';
+    achievements.style.display = 'none';
+    projects.style.display = 'block';
+    projects.scrollIntoView({ behavior: 'smooth' });
+};
 
-				$('.image[data-position]').each(function() {
-
-					var $this = $(this),
-						$img = $this.children('img');
-
-					// Apply img as background.
-						$this
-							.css('background-image', 'url("' + $img.attr('src') + '")')
-							.css('background-position', $this.data('position'))
-							.css('background-size', 'cover')
-							.css('background-repeat', 'no-repeat');
-
-					// Hide img.
-						$img
-							.css('opacity', '0');
-
-				});
-
-			}
-
-	// Header Panel.
-
-		// Nav.
-			var $nav_a = $nav.find('a');
-
-			$nav_a
-				.addClass('scrolly')
-				.on('click', function() {
-
-					var $this = $(this);
-
-					// External link? Bail.
-						if ($this.attr('href').charAt(0) != '#')
-							return;
-
-					// Deactivate all links.
-						$nav_a.removeClass('active');
-
-					// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-						$this
-							.addClass('active')
-							.addClass('active-locked');
-
-				})
-				.each(function() {
-
-					var	$this = $(this),
-						id = $this.attr('href'),
-						$section = $(id);
-
-					// No section for this link? Bail.
-						if ($section.length < 1)
-							return;
-
-					// Scrollex.
-						$section.scrollex({
-							mode: 'middle',
-							top: '5vh',
-							bottom: '5vh',
-							initialize: function() {
-
-								// Deactivate section.
-									$section.addClass('inactive');
-
-							},
-							enter: function() {
-
-								// Activate section.
-									$section.removeClass('inactive');
-
-								// No locked links? Deactivate all links and activate this section's one.
-									if ($nav_a.filter('.active-locked').length == 0) {
-
-										$nav_a.removeClass('active');
-										$this.addClass('active');
-
-									}
-
-								// Otherwise, if this section's link is the one that's locked, unlock it.
-									else if ($this.hasClass('active-locked'))
-										$this.removeClass('active-locked');
-
-							}
-						});
-
-				});
-
-		// Title Bar.
-			$titleBar = $(
-				'<div id="titleBar">' +
-					'<a href="#header" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$header
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'right',
-					target: $body,
-					visibleClass: 'header-visible'
-				});
-
-	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() {
-
-				if (breakpoints.active('<=medium'))
-					return $titleBar.height();
-
-				return 0;
-
-			}
-		});
-
-})(jQuery);
+function displayExtras() {
+    skills.style.display = 'none';
+    about.style.display = 'none';
+    projects.style.display = 'none';
+    achievements.style.display = 'block';
+    achievements.scrollIntoView({ behavior: 'smooth' });
+}
